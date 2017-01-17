3cb4a3d0b205a8eee47646bf740d9c0d80cb1114
qieguo <qieguo.chow@gmail.com>
2017-01-17 23:42:11 +0800

    logs文件夹预处理

--- logger.js
+++ logger.js
@@ -4,17 +4,24 @@
 
 const env = process.env.NODE_ENV || "development";
 const log4js = require('log4js');
+const fs = require('fs');
+const dir = 'logs/'
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir);
+}
+
 
 log4js.configure({
   appenders: [
     {type: 'console'},
-    {type: 'file', filename: 'logs/cheese.log', category: 'cheese'}
+    {type: 'file', filename: dir + 'cheese.log', category: 'cheese'}
   ],
   replaceConsole: true
 });
 
 let logger = log4js.getLogger('cheese');
-logger.setLevel(env !== 'test' ? 'DEBUG' : 'ERROR')
+logger.setLevel(env === 'development' ? 'DEBUG' : 'ERROR')
 
 module.exports = logger;
 
